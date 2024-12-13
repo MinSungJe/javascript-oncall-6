6647de87acd0047e7160a04d6a24c3a2193e1da2
Minje Sung <minje813@dgu.ac.kr>
2024-12-13 21:02:20 +0900

    feat: 근무자 연속 근무 고려
    근무자 배정을 먼저 한 이후 연속 근무 여부를 확인해 순서 변경
    연속 근무 여부는 앞의 날짜부터 확인
    해당 근무일 이후로 해당 근무일의 타입과 맞는 근무자와 순서 변경

--- src/Service/WorkingService.js
+++ src/Service/WorkingService.js
@@ -10,10 +10,52 @@ class WorkingService {
   }
 
   setWorkers() {
+    this.#setInitialWorkers();
+    this.#checkDuplicateWorker();
+  }
+
+  #setInitialWorkers() {
     this.working.days.forEach((day) => {
       if (day.isDayOff) day.setWorker(this.weekendWork.getNextWorker());
       else day.setWorker(this.weekdayWork.getNextWorker());
     });
+  }
+
+  #checkDuplicateWorker() {
+    let previousWorker = '';
+
+    for (let dayIndex = 0; dayIndex < this.working.days.length; dayIndex++) {
+      const currentWorker = this.working.days[dayIndex].worker;
+      if (previousWorker !== currentWorker) {
+        previousWorker = currentWorker;
+        continue;
+      }
+
+      this.#changeWorker(dayIndex);
+      previousWorker = this.working.days[dayIndex].worker;
+    }
+  }
+
+  #changeWorker(dayIndex) {
+    const worker = this.working.days[dayIndex].worker;
+    const isDayoff = this.working.days[dayIndex].isDayOff;
+    const { closeIndex, closeWorker } = this.#getCloseWorker(dayIndex, isDayoff);
+    this.working.days[dayIndex].worker = closeWorker;
+    this.working.days[closeIndex].worker = worker;
+  }
+
+  #getCloseWorker(dayIndex, isDayOff) {
+    let closeIndex = -1;
+    let closeWorker = '';
+
+    for (let index = dayIndex + 1; index < this.working.days.length; index++) {
+      if (this.working.days[index].isDayOff !== isDayOff) continue;
+      closeIndex = index;
+      closeWorker = this.working.days[closeIndex].worker;
+      return { closeIndex, closeWorker };
+    }
+
+    if (closeIndex === -1) return { closeIndex: -1, closeWorker: '민성제' };
   }
 }
 
